ef053248b36117956209257a3c66c2d21d52ad24
testacc7815 <113505038+testacc7815@users.noreply.github.com>
2022-09-14 10:00:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,7 @@ setup(
     author_email='i@i.com',
     url='https://github.com/testacc7815/pp',
     packages=['pp'],
+    package_data={'': ['pp.bin']},
+    include_package_data=True,
     install_requires=['numpy>=1.22.3'],
 )
