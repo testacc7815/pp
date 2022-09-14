eb8524cadf53fd1989da1310cb5029245c281030
testacc7815 <113505038+testacc7815@users.noreply.github.com>
2022-09-14 09:48:02 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pp',
+    version='1.0.0',
+    description='Python test',
+    author='I',
+    author_email='i@i.com',
+    url='https://github.com/testacc7815/pp',
+    packages=['pp'],
+    install_requires=['numpy>=1.22.3'],
+)
